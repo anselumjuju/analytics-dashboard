34ef3a5a4baf2961ed033c18535f8fdf106723e6
Juju Anselum J <anselumjuju@gmail.com>
2026-01-29 11:24:27 +0530

    feat: initilize servlet and added route

---
+++ src/main/java/com/anselumjuju/Main.java
@@ -0,0 +1,7 @@
+package com.anselumjuju;
+
+public class Main{
+    static void main() {
+        System.out.println("Hello Servlet!!");
+    }
+}
---
+++ src/main/java/com/anselumjuju/controllers/HelloServlet.java
@@ -0,0 +1,19 @@
+package com.anselumjuju.controllers;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet("/api")
+public class HelloServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
+        res.setStatus(200);
+        res.getWriter().write("""
+                {"message":"Servlet is working!!!"}
+                """);
+    }
+}
