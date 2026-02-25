3c595b5075d8b3c40694c91378c74006fe12f17e
Juju Anselum J <anselumjuju@gmail.com>
2026-02-25 22:00:04 +0530

    refactor: remove unused files

--- backend-js/server.js
+++
@@ -1,9 +0,0 @@
-import app from './app.js';
-
-const PORT = process.env.PORT || 5000;
-
-console.log('Starting server...');
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
